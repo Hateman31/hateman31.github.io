b67cd7ad25ead990f492b3448cef4e7109942f69
hateman31 <metilogrunge31@gmail.com>
2022-11-03 13:47:09 +0300

    start and finish options

--- cljs-runtime/my_2048.core.js
+++ cljs-runtime/my_2048.core.js
@@ -9,11 +9,11 @@ var y__5134__auto__ = h;
 return ((x__5133__auto__ < y__5134__auto__) ? x__5133__auto__ : y__5134__auto__);
 })());
 })();
-my_2048.core.tile_size = (function (){var f1 = (function (p1__11396_SHARP_){
-return (p1__11396_SHARP_ - (9));
+my_2048.core.tile_size = (function (){var f1 = (function (p1__11523_SHARP_){
+return (p1__11523_SHARP_ - (9));
 });
-var f2 = (function (p1__11397_SHARP_){
-return (p1__11397_SHARP_ / (4));
+var f2 = (function (p1__11524_SHARP_){
+return (p1__11524_SHARP_ / (4));
 });
 return f2(f1(my_2048.core.game.width));
 })();
@@ -29,9 +29,12 @@ return (my_2048.core.score_label.textContent = new_score);
 my_2048.core.game_state = cljs.core.atom.cljs$core$IFn$_invoke$arity$1(my_2048.game.init_state());
 my_2048.core.prev_game_state = cljs.core.atom.cljs$core$IFn$_invoke$arity$1(cljs.core.PersistentVector.EMPTY);
 my_2048.core.score = cljs.core.atom.cljs$core$IFn$_invoke$arity$1(my_2048.game.get_score(cljs.core.deref(my_2048.core.game_state)));
+my_2048.core.ignore_ending = cljs.core.atom.cljs$core$IFn$_invoke$arity$1(false);
+my_2048.core.win_msg = "You won! Do you want to continue?";
+my_2048.core.lose_msg = "You lost! Do you want to play new game?";
 my_2048.core.update_field_BANG_ = (function my_2048$core$update_field_BANG_(direction){
-var shift = (function (p1__11398_SHARP_){
-return my_2048.game.update_grid(p1__11398_SHARP_,direction);
+var shift = (function (p1__11525_SHARP_){
+return my_2048.game.update_grid(p1__11525_SHARP_,direction);
 });
 if(cljs.core.truth_((function (){var and__5043__auto__ = direction;
 if(cljs.core.truth_(and__5043__auto__)){
@@ -46,6 +49,14 @@ return null;
 }
 });
 my_2048.core.undobtn = document.getElementById("undobtn");
+my_2048.core.start_new_game = (function my_2048$core$start_new_game(){
+cljs.core.reset_BANG_(my_2048.core.game_state,my_2048.game.init_state());
+
+return (my_2048.core.undobtn.disabled = true);
+});
+my_2048.core.finish_game = (function my_2048$core$finish_game(){
+return my_2048.core.render_game(cljs.core.repeat.cljs$core$IFn$_invoke$arity$2((4),new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [(0),(0),(0),(0)], null)));
+});
 my_2048.core.main = (function my_2048$core$main(){
 (my_2048.core.undobtn.disabled = true);
 
@@ -54,9 +65,7 @@ my_2048.swipe.arrowSwipe().subscribe(my_2048.core.update_field_BANG_);
 my_2048.swipe.touchSwipe(my_2048.core.game).subscribe(my_2048.core.update_field_BANG_);
 
 module$node_modules$rxjs$dist$cjs$index.fromEvent(document.getElementById("newgamebtn"),"click").subscribe((function (){
-cljs.core.reset_BANG_(my_2048.core.game_state,my_2048.game.init_state());
-
-return (my_2048.core.undobtn.disabled = true);
+return my_2048.core.start_new_game();
 }));
 
 module$node_modules$rxjs$dist$cjs$index.fromEvent(my_2048.core.undobtn,"click").subscribe((function (){
@@ -65,27 +74,46 @@ cljs.core.reset_BANG_(my_2048.core.game_state,cljs.core.deref(my_2048.core.prev_
 return (my_2048.core.undobtn.disabled = true);
 }));
 
-cljs.core.add_watch(my_2048.core.game_state,new cljs.core.Keyword(null,"updating","updating",1454028951),(function (p1__11403_SHARP_,p2__11404_SHARP_,p3__11402_SHARP_,p4__11401_SHARP_){
-my_2048.core.render_game(p4__11401_SHARP_);
+cljs.core.add_watch(my_2048.core.game_state,new cljs.core.Keyword(null,"updating","updating",1454028951),(function (p1__11528_SHARP_,p2__11529_SHARP_,p3__11527_SHARP_,p4__11526_SHARP_){
+cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2([cljs.core.deref(my_2048.core.ignore_ending),my_2048.game.win_QMARK_(p4__11526_SHARP_)], 0));
 
-cljs.core.reset_BANG_(my_2048.core.prev_game_state,p3__11402_SHARP_);
+my_2048.core.render_game(p4__11526_SHARP_);
+
+cljs.core.reset_BANG_(my_2048.core.prev_game_state,p3__11527_SHARP_);
 
 (my_2048.core.undobtn.disabled = false);
 
 cljs.core.swap_BANG_.cljs$core$IFn$_invoke$arity$2(my_2048.core.score,(function (){
-return my_2048.game.get_score(p4__11401_SHARP_);
+return my_2048.game.get_score(p4__11526_SHARP_);
 }));
 
 return my_2048.core.render_score(cljs.core.deref(my_2048.core.score));
 }));
 
-cljs.core.add_watch(my_2048.core.game_state,new cljs.core.Keyword(null,"game-ending","game-ending",-972968126),(function (p1__11406_SHARP_,p2__11407_SHARP_,p3__11408_SHARP_,p4__11405_SHARP_){
-var game_state = p4__11405_SHARP_;
-if(cljs.core.truth_(my_2048.game.win_QMARK_(game_state))){
-return alert("You won!");
+cljs.core.add_watch(my_2048.core.game_state,new cljs.core.Keyword(null,"game-ending","game-ending",-972968126),(function (p1__11531_SHARP_,p2__11532_SHARP_,p3__11533_SHARP_,p4__11530_SHARP_){
+var game_state = p4__11530_SHARP_;
+if(cljs.core.truth_((function (){var and__5043__auto__ = cljs.core.not(cljs.core.deref(my_2048.core.ignore_ending));
+if(and__5043__auto__){
+return my_2048.game.win_QMARK_(game_state);
+} else {
+return and__5043__auto__;
+}
+})())){
+var continue_QMARK_ = confirm(my_2048.core.win_msg);
+if(cljs.core.truth_(continue_QMARK_)){
+return cljs.core.swap_BANG_.cljs$core$IFn$_invoke$arity$2(my_2048.core.ignore_ending,(function (){
+return continue_QMARK_;
+}));
+} else {
+return my_2048.core.finish_game();
+}
 } else {
 if(my_2048.game.lose_QMARK_(game_state)){
-return alert("You lost!");
+if(cljs.core.truth_(confirm(my_2048.core.lose_msg))){
+return my_2048.core.start_new_game();
+} else {
+return my_2048.core.finish_game();
+}
 } else {
 return null;
 }
